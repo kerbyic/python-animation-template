a474a510b515b63e2c6c177c24d6db648c25221b
kerbyic <35430898+kerbyic@users.noreply.github.com>
2022-03-12 22:41:36 -0700

    Update animate.py

--- animate.py
+++ animate.py
@@ -7,18 +7,19 @@ rad = np.linspace(0,6*np.pi,3000)
 xhistory = np.cos(rad)
 yhistory = np.sin(rad)
 
-# Create the figure for animation
-# Use this to adjust the size and resolution of the output file
-fig, ax = plt.subplots(figsize=(5,5))
-fig.set_dpi(150.0)
+# Create the figure for animation. Use this to adjust the size and resolution of the output file (may take experimentation).
+fig, ax = plt.subplots(figsize=(5,5)) # Change fig size to adjust size of figure
+fig.set_dpi(150.0) # Change this to adjust resolution.
 
 # Setup the framerate and evenly pick frames across the available history for target framerate and time
-time = 10 # time for animation in seconds
-framerate = 30 # frames per second
-nframes = len(xhistory)-1 # total number of saved system states
-frames = np.linspace(0, nframes, int(time*framerate), dtype=int) # frame indices to use in the animation
+time = 10 # Time for animation in seconds. Replace with your target time.
+framerate = 30 # Frames per second. Replace with your target rate.
+nframes = len(xhistory)-1 # Total length of list/tuple that contains data. This could be any list/tuple as long as they are all the same size.
 
-# Define how to animate a frame given the index. This is all setup the same way you'd make a normal plot.
+frames = np.linspace(0, nframes, int(time*framerate), dtype=int) # Frame indices to use in the animation. Shouldn't need to edit this line.
+
+# Define how to animate a frame given the index by replacing the contents of this function. 
+# This is all setup the same way you'd make a normal plot.
 def animate(i):
     
     ax.clear() # clear the plot so previous frames don't show up again
@@ -28,7 +29,6 @@ def animate(i):
     ax.set_ylim(-2,2) # set axis bondaries and aspect ratio
     ax.set_xlim(-2,2)
     ax.set_aspect('equal')
-    
     fig.tight_layout() # tight layout so it looks nicer (assuming there are things to arrange)
     
 # Create the animation and save to file. Recommend mp4 over gif (faster render and smaller file size), but requires ffmpeg.
